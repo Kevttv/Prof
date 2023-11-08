66eee2df4b746b40c517de2ab0a07ecc97ce4ae6
Kevin Virgen <kvirgen@misena.edu.co>
2023-11-07 23:03:27 -0500

    add axios

--- app/dashboard/calendar/page.jsx
+++ app/dashboard/calendar/page.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import axios from 'axios'
+
 export default function Calendar() {
+
+    axios.get('https://adso-lookstyle.onrender.com/api/v1/users')
+            .then()
     return (
         <div>
-
+            
         </div>
     )
 }
