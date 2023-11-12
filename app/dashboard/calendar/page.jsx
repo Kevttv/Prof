e6d6a847d27e615a7989efe75976ff226c837365
Kevin Virgen <kvirgen@misena.edu.co>
2023-11-11 21:18:47 -0500

    petition API is not finished

--- app/dashboard/calendar/page.jsx
+++ app/dashboard/calendar/page.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
-
 import axios from 'axios'
 
 export default function Calendar() {
 
     axios.get('https://adso-lookstyle.onrender.com/api/v1/users')
-            .then()
+        .then((response) => {
+            console.log(response.data)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+
+
+
+
     return (
         <div>
-            
+                
         </div>
     )
 }
+
+
+
+// axios.get('/user?ID=12345')
+//   .then(function (response) {
+//     // manejar respuesta exitosa
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     // manejar error
+//     console.log(error);
+//   })
+//   .finally(function () {
+//     // siempre sera executado
+//   });
