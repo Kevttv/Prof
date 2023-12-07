9712b944ceb113329b15caa5b232ab9bae3ebdbf
Kevin <kevttv29@gmail.com>
2023-12-06 22:38:39 -0500

    new changes

--- app/perfil/page.jsx
+++ app/perfil/page.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import UserProfile from '@/components/Profiles/Userprofile'
+
 
 export default function Perfil() {
     return (
         <div>
-                <UserProfile/>
+                gg
         </div>
     )
 }
